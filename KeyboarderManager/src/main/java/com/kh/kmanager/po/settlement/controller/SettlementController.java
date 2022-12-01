35dfa155ae1b04d9837c7896c5da307302d461b4
jackinquantico <jackinquantico@naver.com>
2022-12-01 15:56:25 +0900

    기존 컨트롤러 통합 - 채영

--- KeyboarderManager/src/main/java/com/kh/kmanager/po/settlement/controller/SettlementController.java
+++ KeyboarderManager/src/main/java/com/kh/kmanager/po/settlement/controller/SettlementController.java
@@ -1,5 +1,17 @@
 package com.kh.kmanager.po.settlement.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SettlementController {
 
+	/**
+	 * K-Money 잔액관리 페이지 띄우는 용도 - 채영
+	 * @return
+	 */
+	@RequestMapping("kmoney.po")
+	public String kmoneyMain() {
+		return "po/poSettlement/poKmoneyMain";
+	}
 }
