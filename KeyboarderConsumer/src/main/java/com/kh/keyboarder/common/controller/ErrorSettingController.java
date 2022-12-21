96fa4d1aa1a22a62b9726ddb9c5da6bc7cc330c4
miaarosie <xxxmiaaxxx555@gmail.com>
2022-12-21 15:36:05 +0900

    FO에러페이지(안됨)- 장미

---
+++ KeyboarderConsumer/src/main/java/com/kh/keyboarder/common/controller/ErrorSettingController.java
@@ -0,0 +1,18 @@
+package com.kh.keyboarder.common.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class ErrorSettingController {
+
+	@RequestMapping("/error404")
+	public String Error404(HttpServletResponse res) {
+		return "common/404";
+	}
+	
+	@RequestMapping("/error500")
+	public String Error500(HttpServletResponse res) {
+		return "common/500";
+	}
+}
